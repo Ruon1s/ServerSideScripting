ecfb107d2f7adcfe8c372eb1d313cb78d2e39755
Ruon1s <50231327+Ruon1s@users.noreply.github.com>
2021-03-28 20:12:57 +0300

    server.js

--- week2/server.js
+++ week2/server.js
@@ -1,13 +1,13 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const server = express();
 const db = require('./database/db');
 const cors = require('cors');
 const port = 3000;
 
 db.on('connected', () => {
-    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+    server.listen(port, () => console.log(`Example app listening on port ${port}!`));
     console.log('wee');
 }
 );
@@ -17,12 +17,12 @@ const passport = require('./utils/pass');
 const authRoute = require('./routes/authRoute');
 const catRoute = require('./routes/catRoute');
 const userRoute = require('./routes/userRoute');
-app.use(cors());
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use('/auth', authRoute);
-app.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
-app.use('/user',passport.authenticate('jwt', {session: false}), userRoute);
+server.use(cors());
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+server.use('/auth', authRoute);
+server.use('/cat', passport.authenticate('jwt', {session: false}), catRoute);
+server.use('/user',passport.authenticate('jwt', {session: false}), userRoute);
 
 
 
